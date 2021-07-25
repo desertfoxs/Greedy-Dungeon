d7549a5df7d20de02c358ee9c4a6004c3ecf02e6
Olivetti <angelolivetti78@gmail.com>
2021-07-25 17:13:26 -0300

    arreglado de la camara

---
+++ Assets/Assets-Angel/Scripts-Angel/SalaScritp.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalaScritp : MonoBehaviour
+{
+    public GameObject cameraSala;
+    [Tooltip("ID de la sala")]
+    public int id;
+
+    private GameObject _gameManager;
+    private GameManagerAngel _manager;
+
+    private void Start()
+    {
+        _gameManager = GameObject.FindGameObjectWithTag("GameController");
+        _manager = _gameManager.GetComponent<GameManagerAngel>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("entro a la sala" + id);
+
+        if (collision.transform.tag == "Player")
+        {
+            cameraSala.SetActive(true);
+            _manager.CambioDeSala(id);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            cameraSala.SetActive(false);
+        }
+    }
+}
